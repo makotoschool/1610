a8ddbfed512cbd4d7b83f6db5af35a172f66b3db
704 <makoto@makotodesign.net>
2016-12-01 14:01:42 +0900

    if練習スタート

---
+++ if_practice/js/script.js
@@ -0,0 +1,20 @@
+﻿var myHour =11;
+
+//午前と午後で出力メッセージを変える
+//0-12午前
+//12-24 午後
+
+if(myHour<12){
+document.write('今は午前です');
+
+}else if(myHour==12){
+document.write('今はランチタイムです');
+
+}else{//それ以外の時
+document.write('今は午後です');
+
+}
+
+
+
+
