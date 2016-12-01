fdaac0e8da8de1cfe7d5f9633dd6d2224fb139f7
704 <makoto@makotodesign.net>
2016-12-01 14:34:48 +0900

    営業時間追加

--- if_practice/js/script.js
+++ if_practice/js/script.js
@@ -1,20 +1,22 @@
-﻿var myHour =11;
+﻿var myHour =23;
 
-//午前と午後で出力メッセージを変える
-//0-12午前
-//12-24 午後
 
-if(myHour<12){
-document.write('今は午前です');
+if(myHour>=8 && myHour<=11){
+document.write('今はモーニングです');
 
 }else if(myHour==12){
 document.write('今はランチタイムです');
 
+}else if(myHour>=13 && myHour<=15){
+document.write('今はカフェタイムです');
+
+}else if((myHour>=22 && myHour<24)||(myHour>=0 && myHour<8)){
+document.write('現在閉店中です');
+
 }else{//それ以外の時
-document.write('今は午後です');
+document.write('通常営業');
 
 }
-
 
 
 
