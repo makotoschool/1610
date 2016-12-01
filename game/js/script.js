7e90ebb6777f56f72a52c066b7b9266da446b5a6
704 <makoto@makotodesign.net>
2016-12-01 16:01:15 +0900

    最後にちらっとつくったやつ

---
+++ game/js/script.js
@@ -0,0 +1,31 @@
+﻿var question=prompt('あなたは何月生まれですか？','月を入れてね');
+
+if(question==1){
+ var q_2=prompt('1月の何日生まれですか?','日');
+	if(q_2>=1 && q_2<=15){
+			var reload=confirm('あなたは今月いろんなものを引き寄せます');
+			if(reload){
+				location.reload();	
+				}
+
+	}else{
+			var reload=confirm('体調の変化の月です、宇宙と同調し乗り越えましょう');
+			if(reload){
+				location.reload();	
+				}
+
+	}
+
+}else if(question==2){
+alert('2月生まれのラッキーカラーはピンクです');
+
+}else if(question==3){
+alert('3月生まれのラッキーカラーはブルーです');
+
+}else if(question>12){
+alert('入力にエラーがあります！');
+}
+
+
+
+
---
+++ toggle_control/js/script.js
@@ -0,0 +1,20 @@
+﻿i=0;
+function count(){
+var msg=document.getElementById('msg');
+
+if(i==0){
+msg.innerHTML='いち！';
+i++
+}else if(i==1){
+msg.innerHTML='にーー！';
+i++;
+}else if(i==2){
+msg.innerHTML='さーーん！';
+i=0;
+}
+
+
+
+}
+
+
